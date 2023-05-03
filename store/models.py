1545edbc16c3552cd18ac83abb3d805d0e49b550
Narotsit-Karki <narotsitkarki1234@gmail.com>
2023-05-03 18:00:10 +0545

    Ogani Project Initial Commit

---
+++ store/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+LABELS = (
+    ('hot','Hot'),
+    ('new','New'),
+    ('sale','Sale'),
+    ('default','Default')
+)
+
+STOCK= (
+    ('in_stock','In Stock'),
+    ('out_stock','Out of stock')
+)
+class Department(models.Model):
+    slug = models.CharField(max_length=200,unique=True)
+    dept_name = models.CharField(max_length=200,unique=True)
+
+    def __str__(self):
+        return f"<{self.dept_name}>"
+
+class Category(models.Model):
+    slug = models.CharField(max_length=200,unique=True)
+    cname = models.CharField(max_length=200)
+
+
+
+
+class Products(models.Model):
+    slug = models.CharField(unique=True,max_length=300)
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    picture = models.ImageField(upload_to='media')
+    description = models.TextField()
+    information = models.TextField()
+    weight = models.FloatField()
+    labels = models.CharField(choices=LABELS,max_length=100)
+    department = models.ForeignKey(Department,on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(Category,on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"<{self.name}>:<{id}>"
+
+
---
+++ store/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic.base import View
+
+class BaseView(View):
+    view = dict()
+    view['Departments'] = (
+        'Fresh Meat',
+        'Vegetables',
+        'Fruit and Nut Gifts',
+        'Fresh Berries',
+        'Ocean Foods',
+        'Butter and Eggs',
+        'Fastfood',
+        'Fresh Onion',
+        'Papaya and Crisps',
+        'Oat Meals',
+        'Fresh Bananas'
+    )
+
+class HomeView(BaseView):
+    def get(self,request):
+        self.view
+        self.view['Title'] = 'Home'
+        return render(request,'index.html',self.view)
+
+
+class ContactView(BaseView):
+
+    def get(self,request):
+        self.view
+        return render(request,'contact.html',self.view)
+
+    
