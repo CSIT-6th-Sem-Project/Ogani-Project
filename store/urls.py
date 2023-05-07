c18c415898a1a092fdc1b4304312d766269d9e5d
Narotsit-Karki <narotsitkarki1234@gmail.com>
2023-05-08 00:40:13 +0545

    Added New View and Path
    - Included Cart and Wishlist view.
    - Included Cart and Wishlist path.

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomeView,ContactView
+from .views import HomeView,ContactView,CartView,WishListView
 
 urlpatterns = [
     path('',HomeView.as_view(),name = 'home'),
-    path('/contact-us',ContactView.as_view(),name='contact')
+    path('contact-us',ContactView.as_view(),name='contact'),
+    path('cart',CartView.as_view(),name='cart'),
+    path('wishlist',WishListView.as_view(),name='wishlist')
 ]
--- store/views.py
+++ store/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 from django.views.generic.base import View
 
 class BaseView(View):
     view = dict()
-    view['Departments'] = (
-        'Fresh Meat',
-        'Vegetables',
-        'Fruit and Nut Gifts',
-        'Fresh Berries',
-        'Ocean Foods',
-        'Butter and Eggs',
-        'Fastfood',
-        'Fresh Onion',
-        'Papaya and Crisps',
-        'Oat Meals',
-        'Fresh Bananas'
-    )
+    view['Departments'] = Department.objects.all()
+    view['Categories'] = Category.objects.all()
+    view['Title'] = None
 
 class HomeView(BaseView):
     def get(self,request):
@@ -30,7 +20,22 @@ class ContactView(BaseView):
 
     def get(self,request):
         self.view
+        self.view['Title'] = 'Contact'
         return render(request,'contact.html',self.view)
 
+
+class CartView(BaseView):
+
+    def get(self,request):
+        self.view
+        self.view['Title'] = 'Cart'
+        return render(request,'shopping-cart.html',self.view)
+
+
+class WishListView(BaseView):
+    def get(self,request):
+        self.view
+        self.view['Title'] = 'WishList'
+        return render(request,'shopping-wishlist.html',self.view)
 
 
