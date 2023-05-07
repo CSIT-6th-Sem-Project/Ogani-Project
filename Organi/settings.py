1e6a706efe66f3844f1fdc766c0b4191665a1ea1
Ankit Rai <raia85859@gmail.com>
2023-05-07 20:13:37 +0545

    login and signup

--- Organi/settings.py
+++ Organi/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+LOGIN_REDIRECT_URLS = '/'
+LOGOUT_REDIRECT_URLS= '/'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / "static"]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render ,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 from django.views.generic.base import View
 
@@ -33,4 +34,40 @@ class ContactView(BaseView):
         return render(request,'contact.html',self.view)
 
 
+
+def signup(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        username = request.POST['username'],
+        phone = request.POST['phone'],
+        email = request.POST['email'],
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        if password == cpassword:
+            if User.objects.filter(username = username).exists():
+                messages.error(request,'The username is already taken')
+                return redirect('/signup')
+            elif User.objects.filter(phone = phone).exists():
+                messages.error(request,'The phone number already exists')
+                return redirect('/signup')
+            elif User.objects.filter(email = email).exists():
+                messages.error(request,'The email already exists')
+                return redirect('/signup')
+
+            else:
+                data = User.objects.create(
+                    first_name = first_name,
+                    last_name = last_name,
+                    username = username,
+                    phone  = phone,
+                    email = email,
+                    password = password
+                )
+                data.save()
+        else:
+            messages.error(request,'The password does not match')
+            return redirect('/signup')
+
+    return render(request,'signup.html')
 
