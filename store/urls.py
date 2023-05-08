92a05901329cf1c33ffa2f5fbc42a3e1746cb11b
Narotsit-Karki <narotsitkarki1234@gmail.com>
2023-05-08 07:27:09 +0545

    Added New View and Path in URL
    - Modified Sign Up View
    - Modified Store app paths

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import HomeView,ContactView,CartView,WishListView
+from .views import HomeView,ContactView,CartView,WishListView,signup
 urlpatterns = [
     path('',HomeView.as_view(),name = 'home'),
     path('contact-us',ContactView.as_view(),name='contact'),
     path('cart',CartView.as_view(),name='cart'),
-    path('wishlist',WishListView.as_view(),name='wishlist')
+    path('wishlist',WishListView.as_view(),name='wishlist'),
     path('signup', signup, name='signup')
 ]
