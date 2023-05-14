5d95ddc9351dcfda8f79ec7145ff6253691a6917
Narotsit-Karki <narotsitkarki1234@gmail.com>
2023-05-15 02:11:28 +0545

    Added new Views and Routes and some other features
    - Added neccessary static components for displaying in the BaseView
    - Added cart items add ,  remove cart and update cart views that updates
      many cart items at once
      i

--- store/views.py
+++ store/views.py
@@ -1,19 +1,43 @@
 from .models import *
-from django.shortcuts import render ,redirect
+from django.shortcuts import render ,redirect,get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import messages
 
 # Create your views here.
 from django.views.generic.base import View
 
+
+def convert_to_twodarray(item):
+    gap = 3
+    return [item[i:i + gap] for i in range(0, len(item), gap)]
+
 class BaseView(View):
     view = dict()
     view['Departments'] = Department.objects.all()
     view['Categories'] = Category.objects.all()
+
+
+    latest = Product.objects.filter(labels ='new')
+    view['hot']= Product.objects.filter(labels='hot')
+    # filter products with rating greater than or equal to 4
+    top_rated = Product.objects.filter(rating__gte=4)
+    # filter products with discount greater than 20%
+    discounted = Product.objects.filter(discount__gte=20.0)
+
+    view['Discounted_Products'] = convert_to_twodarray(discounted)
+    view['Latest_Products'] = convert_to_twodarray(latest)
+    view['Top_Rated_Products'] = convert_to_twodarray(top_rated)
+
+
+    view['Featured_Categories'] = Category.objects.filter(labels = 'hot')
+    view['Featured_Products'] = { cat.cname:Product.objects.filter(category = cat) for cat in view['Featured_Categories']}
+
+
     view['Title'] = None
 
 class HomeView(BaseView):
     def get(self,request):
+
         self.view
         self.view['Title'] = 'Home'
         return render(request,'index.html',self.view)
@@ -32,7 +56,55 @@ class CartView(BaseView):
     def get(self,request):
         self.view
         self.view['Title'] = 'Cart'
+        self.view['Cart_Items'] = Cart.objects.filter(user=request.user)
+
         return render(request,'shopping-cart.html',self.view)
+
+
+def add_to_cart(request,slug):
+    product = get_object_or_404(Product,slug = slug)
+    if(not Cart.objects.filter(user = request.user,product=product).exists()):
+        cart_obj = Cart.objects.create(user=request.user,product=product,total=product.price)
+        cart_obj.save()
+        messages.success(request,message=f"{product.name} added to your cart")
+    else:
+        cart = get_object_or_404(Cart,user=request.user,product=product)
+        cart.quantity += 1
+        cart.save()
+        messages.info(request,message=f"{product.name} already added to cart increased quantity by one")
+
+    return redirect(request.META['HTTP_REFERER'])
+
+
+def update_cart(request):
+    if (request.method == "POST"):
+        user_cart = Cart.objects.filter(user = request.user)
+
+        for cart in user_cart:
+            try:
+                new_quantity = int(request.POST[cart.slug])
+
+                if new_quantity <= 0:
+                    cart.delete()
+                elif cart.quantity != new_quantity:
+                    cart.quantity = new_quantity
+                    cart.save()
+
+            except ValueError:
+                messages.error(request,"Please enter a valid quantity")
+                return redirect("cart")
+
+        messages.success(request,"Cart updated Successfully")
+        return redirect("cart")
+
+
+
+
+def remove_from_cart(request,slug):
+    cart = get_object_or_404(Cart,slug=slug,user=request.user)
+    cart.delete()
+    messages.success(request,message=f"{cart.product.name} removed from the cart")
+    return redirect("cart")
 
 
 class WishListView(BaseView):
@@ -76,3 +148,10 @@ def signup(request):
     view['Title'] = 'Signup'
     return render(request,'signup.html',view)
 
+
+
+def search(request):
+    query = request.GET['query']
+    query = query.split('')
+    search_result = Product.objects.filter(name__in=[query]) and Product.objects.filter(description__in=[query])
+    return redirect('/')
